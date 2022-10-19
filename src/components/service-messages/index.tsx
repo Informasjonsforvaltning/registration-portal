362f1a7d272daf7942809b9cf225941011af5767
Jeff Reiffers <jeffreiffers@gmail.com>
2022-10-19 12:46:26 +0200

    chore: strapiv 4 service messages

--- src/components/service-messages/index.tsx
+++ src/components/service-messages/index.tsx
@@ -5,19 +5,44 @@ import { Severity } from '@fellesdatakatalog/alert';
 import env from '../../env';
 import Translation from '../translation';
 
-import { ServiceMessage } from '../../services/api/strapi/generated/graphql';
+import { ServiceMessageEntity } from '../../services/api/strapi/generated/graphql';
 
 import SC from './styled';
 
 interface Props {
-  serviceMessages: ServiceMessage[] | null;
+  serviceMessages: ServiceMessageEntity[] | null;
 }
 
 const { FDK_BASE_URI } = env;
 
+const renderServiceMessage = (entity: ServiceMessageEntity) => {
+  const { id, attributes } = entity;
+  if (attributes) {
+    const { title, short_description, message_type } = attributes;
+    return (
+      <SC.Alert
+        key={id}
+        severity={Severity[message_type as keyof typeof Severity]}
+      >
+        <SC.Content>
+          <SC.Title>{title}</SC.Title>
+          <SC.Description>
+            <SC.Text>{short_description}</SC.Text>
+            <SC.Link href={`${FDK_BASE_URI}/publishing/service-messages/${id}`}>
+              <Translation id='serviceMessagesPage.goToDetailsPage' />
+            </SC.Link>
+          </SC.Description>
+        </SC.Content>
+      </SC.Alert>
+    );
+  }
+
+  return null;
+};
+
 const ServiceMessages: FC<Props> = ({ serviceMessages = [] }) => {
   const [extendedServiceMessages, setExtendedServiceMessages] = useState<
-    ServiceMessage[] | null
+    ServiceMessageEntity[] | null
   >();
 
   useEffect(() => {
@@ -27,26 +52,7 @@ const ServiceMessages: FC<Props> = ({ serviceMessages = [] }) => {
   }, [serviceMessages]);
   return (
     <SC.ServiceMessages>
-      {extendedServiceMessages?.map(
-        ({ id, message_type, title, short_description }) => (
-          <SC.Alert
-            key={id}
-            severity={Severity[message_type as keyof typeof Severity]}
-          >
-            <SC.Content>
-              <SC.Title>{title}</SC.Title>
-              <SC.Description>
-                <SC.Text>{short_description}</SC.Text>
-                <SC.Link
-                  href={`${FDK_BASE_URI}/publishing/service-messages/${id}`}
-                >
-                  <Translation id='serviceMessagesPage.goToDetailsPage' />
-                </SC.Link>
-              </SC.Description>
-            </SC.Content>
-          </SC.Alert>
-        )
-      )}
+      {extendedServiceMessages?.map(entity => renderServiceMessage(entity))}
     </SC.ServiceMessages>
   );
 };
